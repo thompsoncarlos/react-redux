035a2396888e1a12f3d3c4c315cd120da00e3b60
Carlos Thompson <ce-thompson@criticalsotware.com>
2022-03-24 01:50:59 +0000

    react-redux practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import Interval from './components/Interval'
+import Average from './components/Average'
+import Sum from './components/Sum';
+import Random from './components/Random';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React-Redux</h1>
+      <div className='linha'>
+        <Interval></Interval>
+      </div>
+      <div className='linha'>
+        <Sum></Sum>
+        <Average></Average>
+        <Random></Random>
+      </div>
     </div>
   );
 }
